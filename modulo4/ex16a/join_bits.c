827e71b35d9b2695497b4667f446d6208439db2d
FranciscoRedol <1201239@isep.ipp.pt>
2022-01-17 01:01:37 +0000

    Ex16a Corrigido

--- modulo4/ex16a/join_bits.c
+++ modulo4/ex16a/join_bits.c
@@ -1,16 +1,14 @@
 int join_bits(int a, int b, int pos){      
-    int maskB = 0;
-    int maskA = 0;
+    int maskA = 0, maskB = 0xffffffff;
 
     int tempA = a;
     int tempB = b;
 
-    maskA = 1 << (pos + 1);
-    maskA -= 1;
-
-    for(int i = (pos+1); i < 32; i++){
-        maskB = maskB + (1 << i);
+    for (int i = 0; i <= pos; i++){
+        maskA = maskA + (1 << i);
     }
+
+    maskB ^= maskA;
 
     int resultB = tempB & maskB;
     int resultA = tempA & maskA;
---
+++ modulo4/ex16b/asm.s
@@ -0,0 +1,5 @@
+.section .text
+	.global join_bits
+
+join_bits:
+	movl 
